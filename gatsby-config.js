901b7240526f0c449b4bc07fc1979f3d81f76279
James Abaja <jabaja@medgrocer.com>
2020-03-15 13:04:01 +0800

    app(feat): add seo to add page helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,13 @@
 require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `James Abaja`,
+    description: `James' Portfolio Website (v4.0).`,
+    author: `James Abaja <jamesabaja.dev@gmail.com>`,
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-s3`,
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react"
 
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import SEO from "./SEO"
 
 import "../../styles/global.scss"
 
 const Layout = ({ children, scrollToRef, homeRef }) => {
   return (
     <Fragment>
+      <SEO title="James Abaja" />
       <Navbar scrollToRef={scrollToRef} homeRef={homeRef} />
       {children}
       <Footer />
